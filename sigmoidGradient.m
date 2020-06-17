493783944b75ef4799ff7344f3fa75d583cfe900
kk289 <kevilkhadka@gmail.com>
2020-06-16 20:36:14 -0700

    Assign gradient of the sigmoid function

--- sigmoidGradient.m
+++ sigmoidGradient.m
@@ -12,5 +12,6 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-grad = g()
+g = sigmoid(z) .* (1 - sigmoid(z));
+
 end
