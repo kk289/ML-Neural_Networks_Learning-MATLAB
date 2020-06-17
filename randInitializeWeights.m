b7dd7e9bfbbb11a7f8d6caeb05deca10b7c01fc5
kk289 <kevilkhadka@gmail.com>
2020-06-16 20:46:09 -0700

    Update random initializaton

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -1,6 +1,6 @@
 function W = randInitializeWeights(L_in, L_out)
-%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
-%incoming connections and L_out outgoing connections
+% RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+% incoming connections and L_out outgoing connections
 %   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
 %   of a layer with L_in incoming connections and L_out outgoing 
 %   connections. 
@@ -17,16 +17,9 @@ W = zeros(L_out, 1 + L_in);
 %               training the neural network.
 %
 % Note: The first column of W corresponds to the parameters for the bias unit
-%
 
-
-
-
-
-
-
-
-
-% =========================================================================
+% Randomly initialize the weights to small values
+epsilon init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init ? epsilon_init;
 
 end
